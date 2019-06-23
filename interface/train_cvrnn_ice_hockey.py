326b32b74b6dad31a5b6a28867158337af3f1a8d
gla68 <Guiliang>
2019-06-22 21:12:31 -0700

    fix

--- interface/train_cvrnn_ice_hockey.py
+++ interface/train_cvrnn_ice_hockey.py
@@ -296,7 +296,7 @@ def validation_model(testing_dir_games_all, data_store, config, sess, model):
 
 
 def run():
-    test_flag = True
+    test_flag = False
     icehockey_cvrnn_config_path = "../icehockey_cvrnn_config.yaml"
     icehockey_cvrnn_config = CVRNNCongfig.load(icehockey_cvrnn_config_path)
 
