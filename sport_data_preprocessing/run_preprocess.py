89c80735ccad694eeddbb9cf1bd896f645034d45
Guiliang <gla68@sfu.ca>
2019-06-22 21:11:43 -0700

    2019-6-22

--- sport_data_preprocessing/run_preprocess.py
+++ sport_data_preprocessing/run_preprocess.py
@@ -2,15 +2,17 @@ from preprocess import Preprocess
 from build_seq import process_seq_all
 
 if __name__ == '__main__':
-    test_flag = False
+    test_flag = True
     if test_flag:
         hockey_data_dir = '/Users/liu/Desktop/Ice-hokcey-data-sample/data-sample/'
         save_data_dir = '/Users/liu/Desktop/Ice-hokcey-data-sample/feature-sample'
     else:
         hockey_data_dir = '/cs/oschulte/2019-icehockey-data/2018-2019/'
         save_data_dir = '/cs/oschulte/Galen/Ice-hockey-data/2018-2019'
-    # prep = Preprocess(hockey_data_dir=hockey_data_dir, save_data_dir=save_data_dir)
+    prep = Preprocess(hockey_data_dir=hockey_data_dir, save_data_dir=save_data_dir)
     # scaler = prep.scale_allgame_features()
     # wrong_files = prep.process_all(scaler)
     process_seq_all(save_data_dir=save_data_dir)
     # print 'wrong files skipped are {0}'.format(str(wrong_files))
+
+    prep.generate_player_information(store_player_info_dir='../resource/player_info_2018_2019.json')
