168c2fd74ca5b502a682333238de74fa5054fd3b
Guiliang <gla68@sfu.ca>
2019-07-03 18:24:05 -0700

    2019-7-3

--- support/plot_tools.py
+++ support/plot_tools.py
@@ -1,28 +1,28 @@
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 def plot_players_games(match_q_values_players_dict, iteration):
-    matplotlib.pyplot.figure()
+    plt.figure()
     player_ids = match_q_values_players_dict.keys()
     for player_id in player_ids[:3]:
         q_values = match_q_values_players_dict.get(player_id)
+        plt.plot(np.asarray(q_values)[:, 0])
+        plt.savefig('./test_figures/Q_home_iter{0}.png'.format(str(iteration)))
 
-        matplotlib.pyplot.plot(np.asarray(q_values)[:, 0])
-    matplotlib.pyplot.savefig('./test_figures/Q_home_iter{0}.png'.format(str(iteration)))
-
-     matplotlib.pyplot.figure()
+    plt.figure()
     for player_id in player_ids[:3]:
         q_values = match_q_values_players_dict.get(player_id)
 
-        matplotlib.pyplot.plot(np.asarray(q_values)[:, 1])
-    matplotlib.pyplot.savefig('./test_figures/Q_away_iter{0}.png'.format(str(iteration)))
+        plt.plot(np.asarray(q_values)[:, 1])
+    plt.savefig('./test_figures/Q_away_iter{0}.png'.format(str(iteration)))
 
-    matplotlib.pyplot.figure()
+    plt.figure()
     for player_id in player_ids[:3]:
         q_values = match_q_values_players_dict.get(player_id)
 
-        matplotlib.pyplot.plot(np.asarray(q_values)[:, 2])
-    matplotlib.pyplot.savefig('./test_figures/Q_end_iter{0}.png'.format(str(iteration)))
+        plt.plot(np.asarray(q_values)[:, 2])
+    plt.savefig('./test_figures/Q_end_iter{0}.png'.format(str(iteration)))
     pass
