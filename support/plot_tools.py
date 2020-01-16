156dcb0df4cfebc514a51b9581f64e577c0fd5e5
Guiliang <gla68@sfu.ca>
2020-01-16 01:55:26 -0800

    2020-1-16

--- support/plot_tools.py
+++ support/plot_tools.py
@@ -115,9 +115,9 @@ def plot_cv_diff(game_time_list, diff_mean_values_list, diff_var_values_list, mo
             for index in range(int(float(11)/12*len(game_time_list[i])), len(game_time_list[i])):
                 print ('adding value')
                 if diff_mean_values_list[i][index] < 0.5:
-                    np.append(game_time_list_new, game_time_list[i][index])
-                    np.append(diff_mean_values_list_new, diff_mean_values_list[i][index])
-                    np.append(diff_var_values_list_new, diff_var_values_list[i][index])
+                    game_time_list_new = np.append(game_time_list_new, game_time_list[i][index])
+                    diff_mean_values_list_new = np.append(diff_mean_values_list_new, diff_mean_values_list[i][index])
+                    diff_var_values_list_new = np.append(diff_var_values_list_new, diff_var_values_list[i][index])
 
             game_time_list[i] = game_time_list_new
             diff_mean_values_list[i] = diff_mean_values_list_new
